d13ae904ec83b93aa93c8a44298347c64ecf81ff
swamy321 <adsswamy7@gmail.com>
2019-03-03 21:26:24 +0530

    q

--- djproject/testapp/admin.py
+++ djproject/testapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from testapp.models import employee
+
 
 # Register your models here.
+class employeeadmin(admin.ModelAdmin):
+    list_display = ['name','no','sal']
+admin.site.register(employee,employeeadmin)
--- djproject/testapp/models.py
+++ djproject/testapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class employee(models.Model):
+    name=models.CharField(max_length=100)
+    no=models.IntegerField()
+    sal=models.FloatField()
